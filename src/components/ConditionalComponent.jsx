451f958bf2f4d665ff38f5d775fd415ccba55daa
claudiahW <murungiclaud@gmail.com>
2024-01-30 17:56:04 +0300

    display how to use conditionally render list items

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -3,12 +3,7 @@ import Code from "./Code";
 
 export default function ConditionalComponent() {
 
-  let message;
   const display = false;
-  if (display) {
-    message =<h1>This is message one</h1>;
-  }else{
-    message = <h1>This is message two</h1>;
-  }
- return message;   
+ return display ? <Welcome />: <Code />;
+  
 }
