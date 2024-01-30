e232b98c83d77f8963736d0b0efdcadb0b751adc
claudiahW <murungiclaud@gmail.com>
2024-01-30 16:59:22 +0300

    add a new component

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -1,5 +1,5 @@
 export default function ConditionalComponent() {
-    const display = false;
+    const display = true;
     if(display){
         return( <div>
             <h3>This is a Conditional Component</h3>
