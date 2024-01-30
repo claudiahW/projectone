058bf9eb5828812d25bee28e2ce72f76d52c8263
claudiahW <murungiclaud@gmail.com>
2024-01-30 15:54:56 +0300

    create a Fruit component

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,19 +1,22 @@
 export default function Fruits() {
     // const fruits = ["Apple", "Mango", "Orange", "Banana", "Pineapple" ]
-    const fruits =[
-        {name: "Apple",price: 10},
-        {name: "Mango",price: 7},
-        {name: "Banana",price: 2},
-        {name: "Orange",price: 5},
-        {name: "Pinepple",price:},
+    const fruits = [
+        { name: "Apple", price: 10 },
+        { name: "Mango", price: 7 },
+        { name: "Banana", price: 2 },
+        { name: "Orange", price: 5 },
+        { name: "Pinepple", price: 8 },
 
-    ]
-    return ( <div>
+    ];
+    return ( 
+    <div>
         <ul>
         {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
+        <li key={fruit.name}>{fruit.name}  $ {fruit.price}
+        </li>
         ))}
         </ul>
+        
         </div>
-        );
-}
+       
+    )}
