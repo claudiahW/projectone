b49c0e2c489fe9328496017a4b0def4b9e32ea1d
claudiahW <murungiclaud@gmail.com>
2024-01-30 17:29:13 +0300

    display how to use element variables for conditional rendsering

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -1,17 +1,14 @@
+import Welcome from "./Welcome";
+import Code from "./Code";
+
 export default function ConditionalComponent() {
-    const display = true;
-    if(display){
-        return( <div>
-            <h3>This is a Conditional Component</h3>
-        </div>
-        );
-    }else{
-      return(<div>
-        
-        <h3>Code everyday!</h3>
-       
-    </div>
-    ) 
-    }
-   
+
+  let message;
+  const display = false;
+  if (display) {
+    message =<h1>This is message one</h1>;
+  }else{
+    message = <h1>This is message two</h1>;
+  }
+ return message;   
 }
