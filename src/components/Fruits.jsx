1f9e5844bf3c1a666e8534ffafef0417b966c752
claudiahW <murungiclaud@gmail.com>
2024-01-30 15:24:20 +0300

    create an array of objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,5 +1,13 @@
 export default function Fruits() {
-    const fruits = ["Apple", "Mango", "Orange", "Banana", "Pineapple" ]
+    // const fruits = ["Apple", "Mango", "Orange", "Banana", "Pineapple" ]
+    const fruits =[
+        {name: "Apple",price: 10},
+        {name: "Mango",price: 7},
+        {name: "Banana",price: 2},
+        {name: "Orange",price: 5},
+        {name: "Pinepple",price:},
+
+    ]
     return ( <div>
         <ul>
         {fruits.map((fruit) => (
