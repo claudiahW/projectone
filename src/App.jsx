0281fc3537db3fd1cc309dc654ae30217909e034
claudiahW <murungiclaud@gmail.com>
2024-01-31 17:38:29 +0300

    create multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Fruits from "./components/Fruits";
-import Hello from "./components/Hello";
-import Fruit from "./components/Fruit";
-import ConditionalComponent from "./components/ConditionalComponent";
 
+import Counter from "./components/Counter";
 function App() {
   return ( 
   <div className="App">
-   <Fruits />
+   <Counter />
    </div>
       );   
   
